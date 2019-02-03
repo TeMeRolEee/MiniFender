eef4a677b1125639aee7482cc2634d2e6f7b7ddf
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-02-03 19:08:40 +0200

    init method is done, getLatestScanData is in progress as well as getHistory

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -12,11 +12,27 @@ bool DBManager::init() {
     database.setDatabaseName(dataBaseFilePath);
 
     if (database.open()) {
-        QSqlQuery createScanHistoryTable("");
-        return true;
+        QSqlQuery createScanHistoryTable(
+                "CREATE TABLE \"scanHistory\" ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `scanResult` INTEGER NOT NULL DEFAULT 0, `engineResults` TEXT NOT NULL )");
+        /*
+         *  NOT USING AT THE MOMENT
+         *  QSqlQuery createEngineTable(
+         *      "CREATE TABLE IF NOT EXISTS `engines` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `name` TEXT NOT NULL UNIQUE )");
+        */
+        return (createScanHistoryTable.exec());
     }
     qDebug() << "[DBMANAGER]\t" << "Couldn't connect to database";
     return false;
+}
+
+QJsonObject DBManager::getLatestScanData() {
+    QSqlQuery getLatestScan("SELECT scanResult, engineResults, engineResults, scanDate FROM scanHistory LIMIT 1");
+
+    return QJsonObject();
+}
+
+QJsonArray DBManager::getHistory(int lastX) {
+    return QJsonArray();
 }
 
 
--- dbmanager.h
+++ dbmanager.h
@@ -2,12 +2,21 @@
 
 #include <QtCore/QString>
 #include <QtSql/QSqlDatabase>
+#include <QtCore/QJsonObject>
+#include <QtCore/QJsonArray>
 
 class DBManager {
 public:
     DBManager(const QString &path);
 
     bool init();
+
+    bool addScanData(const QJsonObject data);
+
+    QJsonObject getLatestScanData();
+
+    QJsonArray getHistory(int lastX);
+
 private:
     QString dataBaseFilePath;
     QSqlDatabase database;
